99c07f53ccc1cf1c2a0fa58a51c3853859667c7c
Greg Sutcliffe <greg.sutcliffe@gmail.com>
2013-09-12 00:01:56 +0100

    Delete images files when pages are deleted

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+
   def index
     @pages = Page.all
   end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,27 @@
 class Page < ActiveRecord::Base
+
+  STORAGE_DIR = ::Rails.root.join('public','scans')
+  DATE_FORMAT = '%F_%H-%M-%S'
+
+  before_destroy :ensure_file_deleted
+
+  validates_presence_of :label
+  validates_presence_of :path
+  validates_uniqueness_of :path
+
+  private
+
+  def ensure_file_deleted
+    File.unlink(File.join(STORAGE_DIR,path))
+  rescue Errno::ENOENT
+    # already deleted
+    true
+  rescue Exception => e
+    errors.add :base, "Cannot remove file: #{e.message}"
+    Rails.logger.warn e.class
+    Rails.logger.warn e.message
+    Rails.logger.warn e.backtrace
+    false
+  end
+
 end
