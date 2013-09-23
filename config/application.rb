abdd2e3b214539b30199f3c696d32c3b5298a4b0
Greg Sutcliffe <greg.sutcliffe@gmail.com>
2013-09-23 19:52:36 +0100

    Improve css and frontpage

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module Delve
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Precompile some assets
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
