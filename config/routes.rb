2dfdbed213fb400e14623525fa8d7b8b5ea419dc
Greg Sutcliffe <greg.sutcliffe@gmail.com>
2013-09-23 21:57:34 +0100

    Add contact page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Delve::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
+  get "static_pages/contact"
 
   resources :scanners do
     member do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -51,4 +51,17 @@ describe "Static pages" do
 
   end
 
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
+    end
+  end
+
 end
